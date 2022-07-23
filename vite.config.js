288512ca790594dfe09b224232e136d2a52277f7
Lee Froese <design@leefroese.com>
2022-07-23 14:49:42 -0500

    add handlebars for partials

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,18 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import FullReload from 'vite-plugin-full-reload'
 import mpa from 'vite-plugin-mpa'
+import handlebars from 'vite-plugin-handlebars'
 
 export default defineConfig({
   plugins: [
     FullReload(['index.html', 'src/**/*']),
     mpa(),
+    handlebars({
+      partialDirectory: path.resolve(__dirname, './src/partials'),
+      helpers: {
+        ifEquals: (arg1, arg2, options) => (arg1 == arg2) ? options.fn(this) : options.inverse(this),
+      },
+    })
   ],
   resolve: {
     alias: {
